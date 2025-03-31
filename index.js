8f93efad9f420ff568d224cc44d2c6520a1ca003
Delight Bessie <147700339+Bessiedelight@users.noreply.github.com>
2025-03-31 14:18:46 +0000

    origingnnnn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const allowedOrigins = [
   'http://localhost:5000',              // Local development
   'http://localhost:3000',              // Local development
   'https://better-health-live.vercel.app',// Production
-  'https://betterhealthpatient.vercel.app/'
+  'https://betterhealthpatient.vercel.app'
 ];
 
 // Configure CORS to allow multiple origins
